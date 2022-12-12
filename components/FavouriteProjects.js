0bd8d173f86ad05505397f1c041b2fa5b857dba7
salmanhiro <salmanhiro10@gmail.com>
2022-12-12 17:00:13 +0700

    Remove unfinished project

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -73,26 +73,7 @@ export default function FavouriteProjects() {
               </h1>
             </div>
           </a>
-          {/* Single card */}
-          <a
-            href="https://github.com/salmanhiro/basilisk"
-            className="w-full block col-span-3  sm:col-span-2 shadow-2xl"
-          >
-            <div className="relative overflow-hidden">
-              {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
-              <img
-                src="/rokosbasii.jpg"
-                alt="portfolio"
-                className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
-              />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                GPT Roko Basilisk Simulation
-              </h1>
-              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
-                03
-              </h1>
-            </div>
-          </a>
+
         </div>
       </div>
     </div>
