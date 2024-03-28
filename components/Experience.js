61b6442f2ea4ed36fd9558354cfb7ffd1b9877af
salmanhiro <salman.consulting@outlook.com>
2024-03-28 15:54:54 +0700

    background

--- components/Experience.js
+++ components/Experience.js
@@ -24,10 +24,10 @@ export default function Experience() {
               />
               {idx === userData.experience.length - 1 ? null : (
                 <div className="divider-container flex flex-col items-center -mt-2">
-                  <div className="w-4 h-4 bg-green-500 rounded-full relative z-10">
-                    <div className="w-4 h-4 bg-green-500 rounded-full relative z-10 animate-ping"></div>
+                  <div className="w-4 h-4 rounded-full relative z-10">
+                    <div className="w-4 h-4 rounded-full relative z-10 animate-ping"></div>
                   </div>
-                  <div className="w-1 h-24 bg-gray-200 dark:bg-gray-500 rounded-full -mt-2"></div>
+                  <div className="w-1 h-24 -mt-2"></div>
                 </div>
               )}
             </>
@@ -41,7 +41,7 @@ export default function Experience() {
 const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
   return (
     <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
-      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-2xl text-gray-500 font-bold dark:text-gray-800">
+      <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-2xl text-gray-500 font-bold">
         {year}
       </h1>
       <h1 className="font-semibold text-xl">{title}</h1>
